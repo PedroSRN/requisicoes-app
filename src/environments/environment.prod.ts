a246b9db0f7bef120e8b9abeb7531ab76c0a5260
PedroSRN <pedraoflamenguista@hotmail.com>
2022-09-14 00:58:49 -0300

    incluindo  pasta environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBN2KES9bNVhdprFPPqMn3jPM3N3YeZWM4",
+    authDomain: "requisicoes-app-be0d4.firebaseapp.com",
+    projectId: "requisicoes-app-be0d4",
+    storageBucket: "requisicoes-app-be0d4.appspot.com",
+    messagingSenderId: "726144552958",
+    appId: "1:726144552958:web:77dbd4848c99db273b3aaf"
+  }
+
 };
